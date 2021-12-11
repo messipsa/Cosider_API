84a0210a33fa529e062ebae1474561efa7e99a14
Messipsa <im_tamazouzt@esi.dz>
2021-12-11 05:36:59 +0100

    Delete Update GetById

--- controllers/projet.controller.js
+++ controllers/projet.controller.js
@@ -21,3 +21,47 @@ module.exports.addProject = async(req,res)=>{
        return res.status(500).json({message : 'Something went wrong',error : err});
     }
 }
+
+
+module.exports.getProjectById = async(req,res)=>{
+    const id = req.params.id;
+    try{
+       
+          const projet = await Projet.findById(id).orFail();
+          return res.json(projet);
+    }
+    catch(err){
+        return res.status(500).json({message : 'Something went wrong',error : err});
+    }
+}
+
+
+module.exports.Modify = async(req,res)=>{
+    const id = req.params.id;
+    const{lieu , directeur} = req.body;
+    try{
+       const projet = await Projet.findById(id).orFail();
+       projet.lieu = lieu || projet.lieu;
+       projet.directeur = directeur || projet.directeur;
+
+       await projet.save();
+
+       return res.json(projet);
+    }
+    catch(err){
+        return res.status(500).json({message : 'Something went wrong',error : err});
+    }
+}
+
+
+module.exports.deleteProject = async (req,res)=>{
+    const id = req.params.id;
+    try{
+           await Projet.findByIdAndDelete(id).orFail();
+
+           return res.json('user deleted successfully');
+    }
+    catch(err){
+        return res.status(500).json({message : 'Something went wrong',error : err});
+    }
+}
--- routes/projet.routes.js
+++ routes/projet.routes.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const projetController = require('../controllers/projet.controller');
 
 
-router.get('/all' , projetController.getAllProjects);
+router.get('/' , projetController.getAllProjects);
+
+router.get('/:id' , projetController.getProjectById);
 
 router.post('/ajouter' , projetController.addProject);
+
+router.put('/modifier/:id' , projetController.Modify);
+
+router.delete('/supprimer/:id' , projetController.deleteProject);
+
 
 module.exports = router;
