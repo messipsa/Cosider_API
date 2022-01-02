35b099fd33f3deddb538e011254be426feb58aff
Messipsa <im_tamazouzt@esi.dz>
2022-01-02 13:04:55 +0100

    fixing bugs

--- controllers/employe.controller.js
+++ controllers/employe.controller.js
@@ -1,112 +1,119 @@
-const Employe = require('../models/employe');
-const Projet = require('../models/projet');
+const Employe = require("../models/employe");
+const Projet = require("../models/projet");
 
+module.exports.addNewEmployee = async (req, res) => {
+  const {
+    nom,
+    matricule,
+    lieu_naissance,
+    date_naissance,
+    adresse,
+    entite,
+    contrat,
+  } = req.body;
+  try {
+    const project = await Projet.findOne({ entite: entite }).orFail();
+    console.log(project);
+    console.log(project._id);
+    const employe_meme_matricule = await Employe.find({
+      matricule: matricule,
+      projet: project,
+    });
+    console.log(employe_meme_matricule);
+    if (employe_meme_matricule.length !== 0) {
+      return res.status(400).json({ message: "Matricule dupliqué" });
+    }
+    console.log(matricule);
+    const emp = await Employe.create({
+      nom,
+      matricule,
+      lieu_naissance,
+      date_naissance,
+      adresse,
+      projet: project._id,
+      contrat,
+    });
 
-module.exports.addNewEmployee = async(req,res)=>{
-    const {
-        nom , 
-        matricule ,
-         lieu_naissance, 
-         date_naissance , 
-         adresse , 
-         projetId ,  
-         contrat
-         } = req.body;
-         try{
-            const project = await Projet.findById(projetId).orFail();
-            const employe_meme_matricule = await Employe.find({matricule : matricule , projet : project});
-            console.log(employe_meme_matricule);
-            if(employe_meme_matricule.length !==0  )
-            {
-               return res.json({message : 'Matricule dupliqué'});
-            }
-            console.log(matricule)
-           const emp = await Employe.create({nom , matricule , lieu_naissance,date_naissance , adresse , projet : project.id  , contrat})
-          
-           return res.json(emp); 
-        }
-         catch(err){
-            return res.status(500).json({message : 'Something went wrong',error : err});
-         }
-}
+    return res.json(emp);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: err });
+  }
+};
 
+module.exports.getEmployees = async (req, res) => {
+  try {
+    const employes = await Employe.find().populate(
+      "projet",
+      "-__v -createdAt -updatedAt -directeur -lieu"
+    );
+    return res.json(employes);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: err });
+  }
+};
 
-module.exports.getEmployees = async(req,res)=>{
-   try{
-        const employes = await Employe.find().populate('projet' , ('-__v -createdAt -updatedAt -directeur -lieu'));
-        return res.json(employes); 
-   }
-   catch(err){
-      return res.status(500).json({message : 'Something went wrong',error : err});
-   }
-}
+module.exports.getEmployeeById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const employee = await Employe.findById(id)
+      .populate("projet", "-__v -createdAt -updatedAt -directeur -lieu")
+      .orFail();
+    return res.json(employee);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: err });
+  }
+};
 
+module.exports.updateEmployee = async (req, res) => {
+  const id = req.params.id;
+  const { nom, matricule, date_naissance, lieu_naissance, adresse } = req.body;
+  console.log(id);
+  try {
+    const employe = await Employe.findById(id).populate("projet").orFail();
+    console.log(employe);
+    employe.nom = nom || employe.nom;
+    employe.adresse = adresse || employe.adresse;
+    employe.matricule = matricule || employe.matricule;
+    employe.date_naissance = date_naissance || employe.date_naissance;
+    employe.lieu_naissance = lieu_naissance || employe.lieu_naissance;
 
-module.exports.getEmployeeById = async(req,res)=>{
-   const id = req.params.id;
-   try{
-      const employee = await Employe.findById(id)
-      .populate('projet' , '-__v -createdAt -updatedAt -directeur -lieu').orFail();
-      return res.json(employee);
-   }
-   catch(err){
-      return res.status(500).json({message : 'Something went wrong',error : err});
-   }
-}
+    await employe.save();
 
+    return res.json(employe);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", idemp: id, error: err });
+  }
+};
 
-module.exports.updateEmployee = async(req,res)=>{
-   const id= req.params.id;
-   const{nom , matricule , date_naissance , lieu_naissance , adresse } = req.body;
-   try{
-      const employe = await Employe.findById(id).orFail();
-      
-      
-      
-      employe.nom = nom || employe.nom;
-      employe.adresse = adresse ||employe.adresse;
-      employe.matricule = matricule || employe.matricule;
-      employe.date_naissance = date_naissance || employe.date_naissance;
-      employe.lieu_naissance = lieu_naissance || employe.lieu_naissance;
-      
+module.exports.deleteEmployee = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Employe.findByIdAndDelete(id).orFail();
 
-      await employe.save();
+    return res.json("Employe supprimé avec succes");
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: err });
+  }
+};
 
-      return res.json(employe);
-   }
-   catch(err)
-   {
-      return res.status(500).json({message : 'Something went wrong',
-      idemp : id,
-      error : err});
-   }
-}
+module.exports.getEmployeeByEntite = async (req, res) => {
+  const entite = req.params.entite;
+  try {
+    const entity = await Projet.findOne({ entite: entite }).orFail();
+    const employes = await Employe.find({ projet: entity.id });
 
-
-module.exports.deleteEmployee = async(req,res)=>{
-   const id = req.params.id;
-   try{
-     await Employe.findByIdAndDelete(id).orFail();
-
-     return res.json('Employe supprimé avec succes');
-   }
-   catch(err)
-   {
-      return res.status(500).json({message : 'Something went wrong',error : err});
-   }
-}
-
-
-module.exports.getEmployeeByEntite = async(req,res)=>{
-   const entite = req.params.entite;
-   try{
-     const entity = await Projet.findOne({entite : entite}).orFail();
-     const employes = await Employe.find({projet : entity.id});
-   
-     return res.json(employes); 
-   }
-   catch(err)
-   {
-      return res.status(500).json({message : 'Entite inexistante',error : err});
-   }
-}
+    return res.json(employes);
+  } catch (err) {
+    return res.status(500).json({ message: "Entite inexistante", error: err });
+  }
+};
