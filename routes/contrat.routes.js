fe3ff1926747b5141b8358b4ce987158a3d51d82
Messipsa Max <59926380+messipsa@users.noreply.github.com>
2023-04-28 03:31:11 +0200

    Update contrat.routes.js

--- routes/contrat.routes.js
+++ routes/contrat.routes.js
@@ -5,5 +5,5 @@ const auth = require("../middleware/auth");
 router.put("/renouveler/:id", auth.auth, contratController.renouvelerContrat);
 
 router.get("/download/:id", auth.auth, contratController.downloadContrat);
-
+//
 module.exports = router;
